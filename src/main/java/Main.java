25ff646b14c21b801309b2c573c9fcaeba5b3690
Lucas <lucaseduardo101@gmail.com>
2022-06-02 22:38:28 -0300

    Adicionando close ao writer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,12 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         GetMovies movie = new GetMovies();
 
-        HTMLGenerator generator = new HTMLGenerator(new PrintWriter("/home/lucas/Documentos/index.html"));
+        PrintWriter writer = new PrintWriter("/home/lucas/Documentos/index.html");
+
+        HTMLGenerator generator = new HTMLGenerator(writer);
         Response250 response250 = (Response250) movie.getTop250();
         generator.generate(  response250.getItems());
+
+        writer.close();
     }
 }
